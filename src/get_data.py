2288ed0cd26b7df75ff8fe5728983b2ff4527813
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-06-02 08:16:59 -0600

    get_data.py added

---
+++ src/get_data.py
@@ -0,0 +1,21 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config["data_source"]["gdrive_source"]
+    df = pd.read_csv(data_path, sep=',', encoding='utf-8')
+    return df
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default='params.yaml')
+    args_parsed = args.parse_args()
+    data = get_data(config_path=args_parsed.config)
